23fe9df33ac39edcad6e2232926b0994dbdad5f7
Mark <49206900+Menist@users.noreply.github.com>
2026-03-02 17:52:32 +0300

    Update PageNotFound.tsx

--- movie-app/src/pages/PageNotFound/ui/PageNotFound.tsx
+++ movie-app/src/pages/PageNotFound/ui/PageNotFound.tsx
@@ -1,5 +1,5 @@
 import {useNavigate} from "react-router-dom";
-import {Button} from "../../../shared/ui/Button";
+import {Button} from "@/shared/ui/Button";
 import s from './PageNotFound.module.css'
 
 export const PageNotFound = () => {
